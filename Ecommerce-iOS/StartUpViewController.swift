f7e42d3d96c0b090d645e20e4e8fdd203b5034ed
Karthik Venkatesh <kvenkatesh@inkling.com>
2019-05-25 17:48:19 -0700

    Start Up Screen Added

---
+++ Ecommerce-iOS/StartUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StartUpViewController.swift
+//  Panduranga Ecommerce App
+//
+//  Created by Karthik Venkatesh on 5/25/19.
+//  Copyright © 2019 Karthik Venkatesh. All rights reserved.
+//
+
+import UIKit
+
+class StartUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
